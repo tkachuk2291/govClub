c996e7b65a5110b7588d4cb750d1c49343129ef8
Tkachuk <tkacuk2291@gmail.com>
2025-08-04 21:19:56 +0300

    continue to create aside menu

--- govClubFrontEnd/components/asideMenu.vue
+++ govClubFrontEnd/components/asideMenu.vue
@@ -1,25 +1,51 @@
 <script setup lang="ts">
-  import { useMobileMenu } from '@/composables/useMobileMenu'
-  const { isOpen, close } = useMobileMenu()
+import {useMobileMenu} from '@/composables/useMobileMenu'
+
+const {isOpen, close} = useMobileMenu()
 
 
 </script>
 
 <template>
-  <div class="overlay" v-show="isOpen" @click.self="close">
-    <headerMobile />
+  <div class="aside-menu" v-show="isOpen" @click.self="close">
+    <div class="aside-menu-header">
+      <headerMobile/>
+    </div>
+    <div class="aside-menu-content">
+      <ul class="menu">
+        <li class="menu__item">
+          <a class="menu__link" href="#">Напрямки</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Курси</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Команда</a>
+        </li>
+        <li class="menu__item">
+          <a class="menu__link" href="#">Контакти</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="language-container">
+      <p class="language-container__text">Укр</p>
+      <img src="../public/aside-menu/arrow_drop_down.svg" alt="arrow_drop_down" class="language-container__img">
+    </div>
   </div>
 
 </template>
 
 <style scoped lang="scss">
-  .overlay {
+
+
+
+.aside-menu {
   position: fixed;
   inset: 0;
   background-color: white;
   z-index: 1000;
   width: 100%;
-
 }
 
 </style>
